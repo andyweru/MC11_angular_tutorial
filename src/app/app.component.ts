ce8c99a3f277df9a440dd36e703af2ff661cc22f
Andrew Waweru <andyweru@gmail.com>
2018-08-08 00:46:53 +0300

    built in angular pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,4 @@ import {Goal} from './goal'
 })
 export class AppComponent {
 
-  goals = [
-    new Goal(1, 'Watch Finding Nemo'),
-    new Goal(2, 'Buy Cookies'),
-    new Goal(3, 'Get new Phone case'),
-    new Goal(4, 'Get Dog Food'),
-    new Goal(5, 'Solve math homework'),
-    new Goal(6, 'Plot my world domination plan'),
-    new Goal(7, 'debug errors')
-  ];
-
 }
