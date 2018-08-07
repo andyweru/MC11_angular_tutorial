e3c713ff4dd6935e110ca903f2b20af781ae7c61
Andrew Waweru <andyweru@gmail.com>
2018-08-08 01:17:36 +0300

    custom pipe created

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,7 +9,7 @@ import {Goal} from '../goal'
 export class GoalComponent implements OnInit {
 
   goals = [
-    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018,3,14)),
+    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018,7,14)),
     new Goal(2, 'Buy Cookies','I have to buy cookies for the parrot', new Date(2018,6,9)),
     new Goal(3, 'Get new Phone case','Diana has her birthday coming up soon', new Date(2018,1,12)),
     new Goal(4, 'Get Dog Food', 'Pupper likes expensive snacks', new Date(2018,0,18)),
