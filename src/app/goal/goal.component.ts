b2a821b43c25eeb1918e3d6caab37195bb5e5ca5
Andrew Waweru <andyweru@gmail.com>
2018-08-19 10:36:07 +0300

    alertservice imported to goal component and injected to constructor.

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Goal} from '../goal';
 import {Goals} from '../goals';
+
 import {GoalService} from '../goals/goal.service';
+import {AlertsService} from '../alert-service/alerts.service';
 
 @Component({
   selector: 'app-goal',
@@ -12,6 +14,7 @@ import {GoalService} from '../goals/goal.service';
 export class GoalComponent implements OnInit {
 
   goals:Goal[];
+  alertService:AlertsService;
 
   deleteGoal(isComplete, index){
     if(isComplete){
@@ -19,6 +22,7 @@ export class GoalComponent implements OnInit {
 
       if(toDelete){
         this.goals.splice(index, 1)
+        this.alertService.alertMe("Goal has been deleted");
       }
     }
   }
@@ -40,8 +44,9 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal);
   }
 
-  constructor(goalService:GoalService) { 
+  constructor(goalService:GoalService, alertService:AlertsService) { 
     this.goals = goalService.getGoals()
+    this.alertService = alertService;
   }
 
   ngOnInit() {
